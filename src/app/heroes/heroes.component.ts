0fa27157cc0b205f2fc10f8411815c8f83739536
F-Dambrine <cedric.f.pellegrini@gmail.com>
2020-09-15 14:45:51 +0200

    Tour of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import{HeroService} from '../hero.service';
+import { MessageService } from '../message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -12,11 +15,32 @@ export class HeroesComponent implements OnInit {
     name: 'Batman'
   };
 
+  heroes : Hero[];
 
-  constructor() {}
+  selectiedHero: Hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
