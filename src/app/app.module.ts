24852b93cb9671a6583bb86fe0e7da98c2462754
F-Dambrine <cedric.f.pellegrini@gmail.com>
2020-09-16 10:37:16 +0200

    added bootstrap to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoodDetailComponent } from './good-detail/good-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoodDetailComponent
   ],
   imports: [
     BrowserModule,
