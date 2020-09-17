b00853b074542d8279038859c0760ca0e8f5675d
Sophie-Lmr <61805609+Sophie-Lmr@users.noreply.github.com>
2020-09-17 10:55:41 +0200

    modified (bis) HTML and CSS home
    + modification of good detail path in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
-  { path: 'user_details/:id', component: GoodDetailComponent }
+  { path: 'good_details/:id', component: GoodDetailComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
     GoodDetailComponent,
     GoodComponent,
     GoodListComponent,
