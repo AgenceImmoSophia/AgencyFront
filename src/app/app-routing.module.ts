7fa516a2e8666261d9d9b93968ee59a45bae0f2a
Sophie-Lmr <61805609+Sophie-Lmr@users.noreply.github.com>
2020-09-21 17:32:20 +0200

    create sign-in component
    and modified authentication service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import {GoodDetailComponent} from './components/good-detail/good-detail.componen
 import { HomeComponent } from './home/home.component';
 import { GoodListComponent } from './components/good-list/good-list.component';
 import {SellFormComponent} from './form/sell-form/sell-form.component';
+import {SignInComponent} from './sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
+  { path: 'sign-in', component: SignInComponent },
   { path: 'good_details/:id', component: GoodDetailComponent },
   { path: 'goods', component: GoodListComponent },
   { path: 'sell', component: SellFormComponent },
--- src/app/services/authenticationService.ts
+++ src/app/services/authenticationService.ts
@@ -1,35 +1,58 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {map} from 'rxjs/operators';
+import { User } from '../models/user';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  isSignedIn = false;
+  private signInUrl = 'http://localhost:8080/apiUser/';
+  private currentUser: Observable<User>;
+  currentUserSubject: BehaviorSubject<User>;
+  // isSignedIn = false;
+  // httpOptions = {
+  //   headers: new HttpHeaders({'Content-Type': 'application/json'})
+  // };
 
-  constructor(
-    private http: HttpClient) {
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
   }
 
-  signIn(username: string, password: string) {
-    return this.http.post<any>('allUsers/authenticate', {username, password})
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  signIn(username, password) {
+    return this.http.post<any>(this.signInUrl + 'allUsers/authenticate', {username, password})
       .pipe(map(user => {
-        if (user && user.token) {
-          localStorage.setItem('currentUser', JSON.stringify(user));
-          this.isSignedIn = true;
-        }
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        this.currentUserSubject.next(user);
         return user;
       }));
   }
 
+  // signIn(username: string, password: string) {
+  //   return this.http.post<any>(this.signInUrl + 'allUsers/authenticate' + username, password);
+  //     .pipe(map(user => {
+  //       if (user && user.token) {
+  //         localStorage.setItem('currentUser', JSON.stringify(user));
+  //         this.isSignedIn = true;
+  //       }
+  //       return user;
+  //     }));
+  // }
+
   signOut() {
+    return this.http.request<any>(this.signInUrl + '');
     localStorage.removeItem('currentUser');
     this.isSignedIn = false;
   }
 
-  isSignIn() {
+  isSignedIn() {
     // debugger;
     if (JSON.parse(localStorage.getItem('currentUser')).auth_token == null) {
       this.isSignedIn = false;
