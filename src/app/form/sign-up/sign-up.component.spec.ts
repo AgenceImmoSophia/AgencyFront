4d3312f9c9520b37b2d0fa77868821e341f72a2a
Pauline-Humbert <42661326+Pauline-Humbert@users.noreply.github.com>
2020-09-25 16:59:48 +0200

    merge visit

--- src/app/form/sign-up/sign-up.component.spec.ts
+++ src/app/form/sign-up/sign-up.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+<<<<<<< HEAD:src/app/form/sign-up/sign-up.component.spec.ts
 import { SignUpComponent } from './sign-up.component';
 
 describe('SignUpComponent', () => {
@@ -9,12 +10,27 @@ describe('SignUpComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ SignUpComponent ]
+=======
+import { VisitListComponent } from './visit-list.component';
+
+describe('VisitListComponent', () => {
+  let component: VisitListComponent;
+  let fixture: ComponentFixture<VisitListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VisitListComponent ]
+>>>>>>> origin/componentVisit:src/app/components/visit-list/visit-list.component.spec.ts
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+<<<<<<< HEAD:src/app/form/sign-up/sign-up.component.spec.ts
     fixture = TestBed.createComponent(SignUpComponent);
+=======
+    fixture = TestBed.createComponent(VisitListComponent);
+>>>>>>> origin/componentVisit:src/app/components/visit-list/visit-list.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
